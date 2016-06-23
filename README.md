c789b280e82295b72ac5d1b63279e6666422c941
Hernan Y.Ke <yuriqiao@gmail.com>
2016-06-23 15:26:56 -0400

    more properties added

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ position.timestamp
 ```
 ######11 Geolocation basics->demo adding error handling
 content settings-> location->manage exceptions delete local location
+
+######13 more geolocation properties
+```
+position.coords.altitude
+position.coords.altitudeAccuracy
+position.coords.speed
+position.coords.heading
+```
--- script.js
+++ script.js
@@ -5,7 +5,22 @@ function displayLocation(position){
     var lat = position.coords.latitude;
     var long = position.coords.longitude;
     var pLocation = document.getElementById("location");
-    pLocation.innerHTML+=lat+","+long+"<br>";
+    var pInfo = document.getElementById("info");
+    var date = new Date();
+    pLocation.innerHTML+=lat+","+long+"<br>" + date + "<br>" + "accuracy " +position.coords.accuracy+" ";
+    if(position.coords.altitude) {
+        pInfo.innerHTML+=position.coords.altitude+" ";
+    }
+    if(position.coords.altitudeAccuracy) {
+        pInfo.innerHTML+=position.coords.altitudeAccuracy;
+    }
+    if(position.coords.heading) {
+        pInfo.innerHTML+="heading "+position.coords.heading;
+    }
+    if(position.coords.speed) {
+        pInfo.innerHTML+="speed "+position.coords.speed;
+    }
+
 }
 
 function displayEoor(err){
